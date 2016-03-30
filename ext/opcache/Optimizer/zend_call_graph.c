cgxxv/php-src

commit 2737d0cf7cf9e1666c81cb84114f91d94ffa14e8

Added e-SSA based DFA optimisation framework (incomplete)

ext/opcache/Optimizer/zend_call_graph.c
@@ -1,0 +1,304 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine, Call Graph                                              |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1998-2015 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Dmitry Stogov <dmitry@zend.com>                             |
+   +----------------------------------------------------------------------+
+*/
+
+/* $Id:$ */
+
+#include "php.h"
+#include "zend_compile.h"
+#include "zend_extensions.h"
+#include "zend_inference.h"
+#include "zend_call_graph.h"
+#include "zend_func_info.h"
+#include "zend_inference.h"
+#include "zend_call_graph.h"
+
+typedef int (*zend_op_array_func_t)(zend_call_graph *call_graph, zend_op_array *op_array);
+
+static int zend_op_array_calc(zend_call_graph *call_graph, zend_op_array *op_array)
+{
+	(void) op_array;
+
+	call_graph->op_arrays_count++;
+	return SUCCESS;
+}
+
+static int zend_op_array_collect(zend_call_graph *call_graph, zend_op_array *op_array)
+{
+    zend_func_info *func_info = call_graph->func_infos + call_graph->op_arrays_count;
+
+	ZEND_SET_FUNC_INFO(op_array, func_info);
+	call_graph->op_arrays[call_graph->op_arrays_count] = op_array;
+	func_info->num = call_graph->op_arrays_count;
+	func_info->num_args = -1;
+	func_info->return_value_used = -1;
+	call_graph->op_arrays_count++;
+	return SUCCESS;
+}
+
+static int zend_foreach_op_array(zend_call_graph *call_graph, zend_script *script, zend_op_array_func_t func)
+{
+	zend_class_entry *ce;
+	zend_op_array *op_array;
+
+	if (func(call_graph, &script->main_op_array) != SUCCESS) {
+		return FAILURE;
+	}
+
+	ZEND_HASH_FOREACH_PTR(&script->function_table, op_array) {
+		if (func(call_graph, op_array) != SUCCESS) {
+			return FAILURE;
+		}
+	} ZEND_HASH_FOREACH_END();
+
+	ZEND_HASH_FOREACH_PTR(&script->class_table, ce) {
+		ZEND_HASH_FOREACH_PTR(&ce->function_table, op_array) {
+			if (op_array->scope == ce) {
+				if (func(call_graph, op_array) != SUCCESS) {
+					return FAILURE;
+				}
+			}
+		} ZEND_HASH_FOREACH_END();
+	} ZEND_HASH_FOREACH_END();
+
+	return SUCCESS;
+}
+
+static void zend_collect_args_info(zend_call_info *call_info)
+{
+	zend_op *opline = call_info->caller_init_opline;
+	zend_op *end = call_info->caller_call_opline;
+	uint32_t i;
+	int num;
+	int level = 0;
+
+	ZEND_ASSERT(opline && end);
+	if (!opline->extended_value) {
+		return;
+	}
+	for (i = 0; i < opline->extended_value; i++) {
+		call_info->arg_info[i].opline = NULL;
+	}
+	while (opline < end) {
+		opline++;
+		switch (opline->opcode) {
+			case ZEND_SEND_VAL:
+			case ZEND_SEND_VAR:
+			case ZEND_SEND_VAL_EX:
+			case ZEND_SEND_VAR_EX:
+			case ZEND_SEND_REF:
+			case ZEND_SEND_VAR_NO_REF:
+				num = opline->op2.num;
+				if (num > 0) {
+					num--;
+				}
+				if (!level) {
+					call_info->arg_info[num].opline = opline;
+				}
+				break;
+			case ZEND_SEND_ARRAY:
+			case ZEND_SEND_USER:
+			case ZEND_SEND_UNPACK:
+				// ???
+				break;
+			case ZEND_INIT_FCALL:
+			case ZEND_INIT_FCALL_BY_NAME:
+			case ZEND_INIT_NS_FCALL_BY_NAME:
+			case ZEND_INIT_DYNAMIC_CALL:
+			case ZEND_NEW:
+			case ZEND_INIT_METHOD_CALL:
+			case ZEND_INIT_STATIC_METHOD_CALL:
+			case ZEND_INIT_USER_CALL:
+				level++;
+				break;
+			case ZEND_DO_FCALL:
+			case ZEND_DO_ICALL:
+			case ZEND_DO_UCALL:
+			case ZEND_DO_FCALL_BY_NAME:
+				level--;
+				break;
+		}
+	}
+}
+
+static int zend_analyze_calls(zend_arena **arena, zend_script *script, uint32_t build_flags, zend_op_array *op_array, zend_func_info *func_info)
+{
+	zend_op *opline = op_array->opcodes;
+	zend_op *end = opline + op_array->last;
+	zend_function *func;
+	zend_call_info *call_info;
+	int call = 0;
+	zend_call_info **call_stack = alloca((op_array->last / 2) * sizeof(zend_call_info*));
+
+	while (opline != end) {
+		call_info = NULL;
+		switch (opline->opcode) {
+			case ZEND_INIT_FCALL:
+				if ((func = zend_hash_find_ptr(&script->function_table, Z_STR_P(CRT_CONSTANT(opline->op2)))) != NULL) {
+					zend_func_info *callee_func_info = ZEND_FUNC_INFO(&func->op_array);
+					if (callee_func_info) {
+					    call_info = zend_arena_calloc(arena, 1, sizeof(zend_call_info) + (sizeof(zend_send_arg_info) * ((int)opline->extended_value - 1)));
+						call_info->caller_op_array = op_array;
+						call_info->caller_init_opline = opline;
+						call_info->caller_call_opline = NULL;
+						call_info->callee_func = func;
+						call_info->num_args = opline->extended_value;
+						call_info->next_caller = callee_func_info->caller_info;
+						callee_func_info->caller_info = call_info;
+						call_info->next_callee = func_info->callee_info;
+						func_info->callee_info = call_info;
+					}
+				} else if ((func = zend_hash_find_ptr(EG(function_table), Z_STR_P(CRT_CONSTANT(opline->op2)))) != NULL &&
+				           func->type == ZEND_INTERNAL_FUNCTION) {
+					call_info = zend_arena_calloc(arena, 1, sizeof(zend_call_info) + (sizeof(zend_send_arg_info) * ((int)opline->extended_value - 1)));
+					call_info->caller_op_array = op_array;
+					call_info->caller_init_opline = opline;
+					call_info->caller_call_opline = NULL;
+					call_info->callee_func = func;
+					call_info->num_args = opline->extended_value;
+					call_info->next_caller = NULL;
+					call_info->next_callee = func_info->callee_info;
+					func_info->callee_info = call_info;
+				}
+				/* break missing intentionally */
+			case ZEND_INIT_FCALL_BY_NAME:
+			case ZEND_INIT_NS_FCALL_BY_NAME:
+			case ZEND_INIT_DYNAMIC_CALL:
+			case ZEND_NEW:
+			case ZEND_INIT_METHOD_CALL:
+			case ZEND_INIT_STATIC_METHOD_CALL:
+			case ZEND_INIT_USER_CALL:
+				call_stack[call] = call_info;
+				call++;
+				break;
+			case ZEND_DO_FCALL:
+			case ZEND_DO_ICALL:
+			case ZEND_DO_UCALL:
+			case ZEND_DO_FCALL_BY_NAME:
+				func_info->flags |= ZEND_FUNC_HAS_CALLS;
+				call--;
+				if (call_stack[call]) {
+					call_stack[call]->caller_call_opline = opline;
+					zend_collect_args_info(call_stack[call]);
+				}
+				break;
+		}
+		opline++;
+	}
+	return SUCCESS;
+}
+
+static int zend_is_indirectly_recursive(zend_op_array *root, zend_op_array *op_array, zend_bitset visited)
+{
+	zend_func_info *func_info;
+	zend_call_info *call_info;
+	int ret = 0;
+
+	if (op_array == root) {
+		return 1;
+	}
+
+	func_info = ZEND_FUNC_INFO(op_array);
+	if (zend_bitset_in(visited, func_info->num)) {
+		return 0;
+	}
+	zend_bitset_incl(visited, func_info->num);
+	call_info = func_info->caller_info;
+	while (call_info) {
+		if (zend_is_indirectly_recursive(root, call_info->caller_op_array, visited)) {
+			call_info->recursive = 1;
+			ret = 1;
+		}
+		call_info = call_info->next_caller;
+	}
+	return ret;
+}
+
+static void zend_analyze_recursion(zend_call_graph *call_graph)
+{
+	zend_op_array *op_array;
+	zend_func_info *func_info;
+	zend_call_info *call_info;
+	int i;
+	int set_len = zend_bitset_len(call_graph->op_arrays_count);
+	zend_bitset visited;
+	ALLOCA_FLAG(use_heap);
+
+	visited = ZEND_BITSET_ALLOCA(set_len, use_heap);
+	for (i = 0; i < call_graph->op_arrays_count; i++) {
+		op_array = call_graph->op_arrays[i];
+		func_info = call_graph->func_infos + i;
+		call_info = func_info->caller_info;
+		while (call_info) {
+			if (call_info->caller_op_array == op_array) {
+				call_info->recursive = 1;
+				func_info->flags |= ZEND_FUNC_RECURSIVE | ZEND_FUNC_RECURSIVE_DIRECTLY;
+			} else {
+				memset(visited, 0, sizeof(uint32_t) * set_len);
+				if (zend_is_indirectly_recursive(op_array, call_info->caller_op_array, visited)) {
+					call_info->recursive = 1;
+					func_info->flags |= ZEND_FUNC_RECURSIVE | ZEND_FUNC_RECURSIVE_INDIRECTLY;
+				}
+			}
+			call_info = call_info->next_caller;
+		}
+	}
+}
+
+static void zend_sort_op_arrays(zend_call_graph *call_graph)
+{
+	(void) call_graph;
+
+	// TODO: perform topological sort of cyclic call graph
+}
+
+int zend_build_call_graph(zend_arena **arena, zend_script *script, uint32_t build_flags, zend_call_graph *call_graph) /* {{{ */
+{
+	int i;
+
+	call_graph->op_arrays_count = 0;
+	if (zend_foreach_op_array(call_graph, script, zend_op_array_calc) != SUCCESS) {
+		return FAILURE;
+	}
+	call_graph->op_arrays  = (zend_op_array**)zend_arena_calloc(arena, call_graph->op_arrays_count, sizeof(zend_op_array*));
+	call_graph->func_infos = (zend_func_info*)zend_arena_calloc(arena, call_graph->op_arrays_count, sizeof(zend_func_info));
+	if (!call_graph->op_arrays || !call_graph->func_infos) {
+		return FAILURE;
+	}
+
+	call_graph->op_arrays_count = 0;
+	if (zend_foreach_op_array(call_graph, script, zend_op_array_collect) != SUCCESS) {
+		return FAILURE;
+	}
+
+	for (i = 0; i < call_graph->op_arrays_count; i++) {
+		zend_analyze_calls(arena, script, build_flags, call_graph->op_arrays[i], call_graph->func_infos + i);
+	}
+	zend_analyze_recursion(call_graph);
+	zend_sort_op_arrays(call_graph);
+
+	return SUCCESS;
+}
+/* }}} */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ */

ext/opcache/Optimizer/block_pass.c
@@ -1745,13 +1745,13 @@ void optimize_cfg(zend_op_array *op_array, zend_optimizer_ctx *ctx)
 
     /* Build CFG */
 	checkpoint = zend_arena_checkpoint(ctx->arena);
-	if (zend_build_cfg(&ctx->arena, op_array, 0, 0, &cfg, NULL) != SUCCESS) {
+	if (zend_build_cfg(&ctx->arena, op_array, 0, &cfg, NULL) != SUCCESS) {
 		zend_arena_release(&ctx->arena, checkpoint);
 		return;
 	}
 
 	if (ctx->debug_level & ZEND_DUMP_BEFORE_BLOCK_PASS) {
-		zend_dump_op_array(op_array, &cfg, ZEND_DUMP_UNREACHABLE, "before block pass");
+		zend_dump_op_array(op_array, ZEND_DUMP_CFG, "before block pass", &cfg);
 	}
 
 	if (op_array->last_var || op_array->T) {
@@ -1805,7 +1805,7 @@ void optimize_cfg(zend_op_array *op_array, zend_optimizer_ctx *ctx)
 	assemble_code_blocks(&cfg, op_array);
 
 	if (ctx->debug_level & ZEND_DUMP_AFTER_BLOCK_PASS) {
-		zend_dump_op_array(op_array, &cfg, 0, "after block pass");
+		zend_dump_op_array(op_array, ZEND_DUMP_CFG | ZEND_DUMP_HIDE_UNREACHABLE, "after block pass", &cfg);
 	}
 
 	/* Destroy CFG */

ext/opcache/Optimizer/dfa_pass.c
@@ -26,6 +26,8 @@
 #include "zend_bitset.h"
 #include "zend_cfg.h"
 #include "zend_ssa.h"
+#include "zend_func_info.h"
+#include "zend_inference.h"
 #include "zend_dump.h"
 
 #ifndef HAVE_DFA_PASS
@@ -35,8 +37,8 @@
 void optimize_dfa(zend_op_array *op_array, zend_optimizer_ctx *ctx)
 {
 	void *checkpoint;
-	uint32_t flags;
-	zend_cfg cfg;
+	uint32_t build_flags;
+	uint32_t flags = 0;
 	zend_ssa ssa;
 
 #if !HAVE_DFA_PASS
@@ -44,9 +46,10 @@ void optimize_dfa(zend_op_array *op_array, zend_optimizer_ctx *ctx)
 #endif
 
     /* Build SSA */
+	memset(&ssa, 0, sizeof(ssa));
 	checkpoint = zend_arena_checkpoint(ctx->arena);
 
-	if (zend_build_cfg(&ctx->arena, op_array, 0, 0, &cfg, &flags) != SUCCESS) {
+	if (zend_build_cfg(&ctx->arena, op_array, 0, &ssa.cfg, &flags) != SUCCESS) {
 		zend_arena_release(&ctx->arena, checkpoint);
 		return;
 	}
@@ -56,52 +59,79 @@ void optimize_dfa(zend_op_array *op_array, zend_optimizer_ctx *ctx)
 		return;
 	}
 
-	if (zend_cfg_build_predecessors(&ctx->arena, &cfg) != SUCCESS) {
+	if (zend_cfg_build_predecessors(&ctx->arena, &ssa.cfg) != SUCCESS) {
 		zend_arena_release(&ctx->arena, checkpoint);
 		return;
 	}
 
 	if (ctx->debug_level & ZEND_DUMP_DFA_CFG) {
-		zend_dump_op_array(op_array, &cfg, 0, "dfa cfg");
+		zend_dump_op_array(op_array, ZEND_DUMP_CFG | ZEND_DUMP_HIDE_UNUSED_VARS, "dfa cfg", &ssa.cfg);
 	}
 
 	/* Compute Dominators Tree */
-	if (zend_cfg_compute_dominators_tree(op_array, &cfg) != SUCCESS) {
+	if (zend_cfg_compute_dominators_tree(op_array, &ssa.cfg) != SUCCESS) {
 		zend_arena_release(&ctx->arena, checkpoint);
 		return;
 	}
 
 	/* Identify reducible and irreducible loops */
-	if (zend_cfg_identify_loops(op_array, &cfg, &flags) != SUCCESS) {
+	if (zend_cfg_identify_loops(op_array, &ssa.cfg, &flags) != SUCCESS) {
 		zend_arena_release(&ctx->arena, checkpoint);
 		return;
 	}
 
 	if (ctx->debug_level & ZEND_DUMP_DFA_DOMINATORS) {
-		int j;
-
-		fprintf(stderr, "DOMINATORS-TREE:\n");
-		for (j = 0; j < cfg.blocks_count; j++) {
-			zend_basic_block *b = cfg.blocks + j;
-			if (b->flags & ZEND_BB_REACHABLE) {
-				zend_dump_block_info(&cfg, j, 0);
-			}
-		}
+		zend_dump_dominators(op_array, &ssa.cfg);
 	}
 
-	if (zend_build_ssa(&ctx->arena, op_array, &cfg, 0, &ssa, &flags) != SUCCESS) {
+	build_flags = 0;
+	if (ctx->debug_level & ZEND_DUMP_DFA_LIVENESS) {
+		build_flags |= ZEND_SSA_DEBUG_LIVENESS;
+	}
+	if (ctx->debug_level & ZEND_DUMP_DFA_PHI) {
+		build_flags |= ZEND_SSA_DEBUG_PHI_PLACEMENT;
+	}
+	if (zend_build_ssa(&ctx->arena, op_array, build_flags, &ssa, &flags) != SUCCESS) {
 		zend_arena_release(&ctx->arena, checkpoint);
 		return;
 	}
 
-	if (ctx->debug_level & ZEND_DUMP_BEFORE_DFA_PASS) {
-		zend_dump_op_array(op_array, &cfg, ZEND_DUMP_UNREACHABLE, "before dfa pass");
+	if (ctx->debug_level & ZEND_DUMP_DFA_SSA) {
+		zend_dump_op_array(op_array, ZEND_DUMP_SSA | ZEND_DUMP_HIDE_UNUSED_VARS, "before dfa pass", &ssa);
 	}
 
-	//TODO: ???
+
+	if (zend_ssa_compute_use_def_chains(&ctx->arena, op_array, &ssa) != SUCCESS){
+		zend_arena_release(&ctx->arena, checkpoint);
+		return;
+	}
+
+	if (zend_ssa_find_false_dependencies(op_array, &ssa) != SUCCESS) {
+		zend_arena_release(&ctx->arena, checkpoint);
+		return;
+	}
+
+	if (zend_ssa_find_sccs(op_array, &ssa) != SUCCESS){
+		zend_arena_release(&ctx->arena, checkpoint);
+		return;
+	}
+
+	if (zend_ssa_inference(&ctx->arena, op_array, ctx->script, &ssa) != SUCCESS) {
+		return;
+	}
+
+	if (ctx->debug_level & ZEND_DUMP_DFA_SSA_VARS) {
+		zend_dump_ssa_variables(op_array, &ssa);
+	}
+
+	if (ctx->debug_level & ZEND_DUMP_BEFORE_DFA_PASS) {
+		zend_dump_op_array(op_array, ZEND_DUMP_SSA | ZEND_DUMP_HIDE_UNUSED_VARS, "before dfa pass", &ssa);
+	}
+
+	//TODO: Add optimization???
 
 	if (ctx->debug_level & ZEND_DUMP_AFTER_DFA_PASS) {
-		zend_dump_op_array(op_array, &cfg, 0, "after dfa pass");
+		zend_dump_op_array(op_array, ZEND_DUMP_SSA | ZEND_DUMP_HIDE_UNUSED_VARS, "after dfa pass", &ssa);
 	}
 
 	/* Destroy SSA */

ext/opcache/Optimizer/zend_call_graph.h
@@ -1,0 +1,83 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine, Call Graph                                              |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1998-2015 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Dmitry Stogov <dmitry@zend.com>                             |
+   +----------------------------------------------------------------------+
+*/
+
+#ifndef ZEND_CALL_GRAPH_H
+#define ZEND_CALL_GRAPH_H
+
+#include "zend_ssa.h"
+#include "zend_func_info.h"
+#include "zend_optimizer.h"
+
+typedef struct _zend_send_arg_info {
+	zend_op                *opline;
+} zend_send_arg_info;
+
+typedef struct _zend_recv_arg_info {
+	int                     ssa_var;
+	zend_ssa_var_info       info;
+} zend_recv_arg_info;
+
+struct _zend_call_info {
+	zend_op_array          *caller_op_array;
+	zend_op                *caller_init_opline;
+	zend_op                *caller_call_opline;
+	zend_function          *callee_func;
+	zend_call_info         *next_caller;
+	zend_call_info         *next_callee;
+	zend_func_info         *clone;
+	int                     recursive;
+	int                     num_args;
+	zend_send_arg_info      arg_info[1];
+};
+
+struct _zend_func_info {
+	int                     num;
+	uint32_t                flags;
+	zend_ssa                ssa;          /* Static Single Assignmnt Form  */
+	zend_call_info         *caller_info;  /* where this function is called from */
+	zend_call_info         *callee_info;  /* which functions are called from this one */
+	int                     num_args;     /* (-1 - unknown) */
+	zend_recv_arg_info     *arg_info;
+	zend_ssa_var_info       return_info;
+	zend_func_info         *clone;
+	int                     clone_num;
+	int                     return_value_used; /* -1 unknown, 0 no, 1 yes */
+	void                   *codegen_data;
+};
+
+typedef struct _zend_call_graph {
+	int                     op_arrays_count;
+	zend_op_array         **op_arrays;
+	zend_func_info         *func_infos;
+} zend_call_graph;
+
+BEGIN_EXTERN_C()
+
+int zend_build_call_graph(zend_arena **arena, zend_script *script, uint32_t build_flags, zend_call_graph *call_graph);
+
+END_EXTERN_C()
+
+#endif /* ZEND_CALL_GRAPH_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ */

ext/opcache/Optimizer/zend_cfg.c
@@ -19,6 +19,7 @@
 #include "php.h"
 #include "zend_compile.h"
 #include "zend_cfg.h"
+#include "zend_func_info.h"
 #include "zend_worklist.h"
 
 static void zend_mark_reachable(zend_op *opcodes, zend_basic_block *blocks, zend_basic_block *b) /* {{{ */
@@ -75,7 +76,7 @@ static void zend_mark_reachable(zend_op *opcodes, zend_basic_block *blocks, zend
 }
 /* }}} */
 
-static void zend_mark_reachable_blocks(zend_op_array *op_array, zend_cfg *cfg, int start) /* {{{ */
+static void zend_mark_reachable_blocks(const zend_op_array *op_array, zend_cfg *cfg, int start) /* {{{ */
 {
 	zend_basic_block *blocks = cfg->blocks;
 
@@ -199,7 +200,7 @@ static void zend_mark_reachable_blocks(zend_op_array *op_array, zend_cfg *cfg, i
 }
 /* }}} */
 
-void zend_cfg_remark_reachable_blocks(zend_op_array *op_array, zend_cfg *cfg) /* {{{ */
+void zend_cfg_remark_reachable_blocks(const zend_op_array *op_array, zend_cfg *cfg) /* {{{ */
 {
 	zend_basic_block *blocks = cfg->blocks;
 	int i;
@@ -232,7 +233,7 @@ static void record_successor(zend_basic_block *blocks, int pred, int n, int succ
 		block_map[i] = 1; \
 	} while (0)
 
-int zend_build_cfg(zend_arena **arena, zend_op_array *op_array, int rt_constants, int stackless, zend_cfg *cfg, uint32_t *func_flags) /* {{{ */
+int zend_build_cfg(zend_arena **arena, const zend_op_array *op_array, uint32_t build_flags, zend_cfg *cfg, uint32_t *func_flags) /* {{{ */
 {
 	uint32_t flags = 0;
 	uint32_t i;
@@ -270,7 +271,7 @@ int zend_build_cfg(zend_arena **arena, zend_op_array *op_array, int rt_constants
 			case ZEND_YIELD:
 			case ZEND_YIELD_FROM:
 				flags |= ZEND_FUNC_TOO_DYNAMIC;
-				if (stackless) {
+				if (build_flags & ZEND_CFG_STACKLESS) {
 					BB_START(i + 1);
 				}
 				break;
@@ -278,7 +279,7 @@ int zend_build_cfg(zend_arena **arena, zend_op_array *op_array, int rt_constants
 			case ZEND_DO_UCALL:
 			case ZEND_DO_FCALL_BY_NAME:
 				flags |= ZEND_FUNC_HAS_CALLS;
-				if (stackless) {
+				if (build_flags & ZEND_CFG_STACKLESS) {
 					BB_START(i + 1);
 				}
 				break;
@@ -532,7 +533,7 @@ int zend_build_cfg(zend_arena **arena, zend_op_array *op_array, int rt_constants
 	zend_mark_reachable_blocks(op_array, cfg, 0);
 
 	if (func_flags) {
-		*func_flags = flags;
+		*func_flags |= flags;
 	}
 
 	return SUCCESS;
@@ -602,7 +603,7 @@ int zend_cfg_build_predecessors(zend_arena **arena, zend_cfg *cfg) /* {{{ */
 }
 /* }}} */
 
-int zend_cfg_compute_dominators_tree(zend_op_array *op_array, zend_cfg *cfg) /* {{{ */
+int zend_cfg_compute_dominators_tree(const zend_op_array *op_array, zend_cfg *cfg) /* {{{ */
 {
 	zend_basic_block *blocks = cfg->blocks;
 	int blocks_count = cfg->blocks_count;
@@ -694,7 +695,7 @@ static int dominates(zend_basic_block *blocks, int a, int b) /* {{{ */
 }
 /* }}} */
 
-int zend_cfg_identify_loops(zend_op_array *op_array, zend_cfg *cfg, uint32_t *flags) /* {{{ */
+int zend_cfg_identify_loops(const zend_op_array *op_array, zend_cfg *cfg, uint32_t *flags) /* {{{ */
 {
 	int i, j, k;
 	int depth;

ext/opcache/Optimizer/zend_cfg.h
@@ -19,13 +19,6 @@
 #ifndef ZEND_CFG_H
 #define ZEND_CFG_H
 
-/* func flags */
-#define ZEND_FUNC_TOO_DYNAMIC    (1<<0)
-#define ZEND_FUNC_HAS_CALLS      (1<<1)
-#define ZEND_FUNC_VARARG         (1<<2)
-#define ZEND_FUNC_NO_LOOPS       (1<<3)
-#define ZEND_FUNC_IRREDUCIBLE    (1<<4)
-
 /* zend_basic_bloc.flags */
 #define ZEND_BB_START            (1<<0)  /* fist block             */
 #define ZEND_BB_FOLLOW           (1<<1)  /* follows the next block */
@@ -95,20 +88,32 @@ typedef struct _zend_cfg {
 	uint32_t         *map;
 } zend_cfg;
 
-#define CRT_CONSTANT(node) \
-	(rt_constants ? \
+/* Build Flags */
+#define ZEND_RT_CONSTANTS              (1<<31)
+#define ZEND_CFG_STACKLESS             (1<<30)
+#define ZEND_SSA_DEBUG_LIVENESS        (1<<29)
+#define ZEND_SSA_DEBUG_PHI_PLACEMENT   (1<<28)
+
+#define CRT_CONSTANT_EX(op_array, node, rt_constants) \
+	((rt_constants) ? \
 		RT_CONSTANT(op_array, (node)) \
 	: \
 		CT_CONSTANT_EX(op_array, (node).constant) \
 	)
 
+#define CRT_CONSTANT(node) \
+	CRT_CONSTANT_EX(op_array, node, (build_flags & ZEND_RT_CONSTANTS))
+
+#define RETURN_VALUE_USED(opline) \
+	(!((opline)->result_type & EXT_TYPE_UNUSED))
+
 BEGIN_EXTERN_C()
 
-int  zend_build_cfg(zend_arena **arena, zend_op_array *op_array, int rt_constants, int stackless, zend_cfg *cfg, uint32_t *func_flags);
-void zend_cfg_remark_reachable_blocks(zend_op_array *op_array, zend_cfg *cfg);
+int  zend_build_cfg(zend_arena **arena, const zend_op_array *op_array, uint32_t build_flags, zend_cfg *cfg, uint32_t *func_flags);
+void zend_cfg_remark_reachable_blocks(const zend_op_array *op_array, zend_cfg *cfg);
 int  zend_cfg_build_predecessors(zend_arena **arena, zend_cfg *cfg);
-int  zend_cfg_compute_dominators_tree(zend_op_array *op_array, zend_cfg *cfg);
-int  zend_cfg_identify_loops(zend_op_array *op_array, zend_cfg *cfg, uint32_t *flags);
+int  zend_cfg_compute_dominators_tree(const zend_op_array *op_array, zend_cfg *cfg);
+int  zend_cfg_identify_loops(const zend_op_array *op_array, zend_cfg *cfg, uint32_t *flags);
 
 END_EXTERN_C()
 

ext/opcache/Optimizer/zend_dfg.c
@@ -20,7 +20,7 @@
 #include "zend_compile.h"
 #include "zend_dfg.h"
 
-int zend_build_dfg(zend_op_array *op_array, zend_cfg *cfg, zend_dfg *dfg) /* {{{ */
+int zend_build_dfg(const zend_op_array *op_array, const zend_cfg *cfg, zend_dfg *dfg) /* {{{ */
 {
 	int set_size;
 	zend_basic_block *blocks = cfg->blocks;
@@ -222,18 +222,6 @@ int zend_build_dfg(zend_op_array *op_array, zend_cfg *cfg, zend_dfg *dfg) /* {{{
 			}
 		}
 	} while (changed);
-
-//???D	if (ZCG(accel_directives).jit_debug & JIT_DEBUG_DUMP_LIVENESS) {
-//???D		fprintf(stderr, "Variable Liveness\n");
-//???D		for (j = 0; j < blocks_count; j++) {
-//???D			fprintf(stderr, "  BB%d:\n", j);
-//???D			zend_jit_dump_var_set(op_array, "gen", dfg->gen + (j * dfg->size));
-//???D			zend_jit_dump_var_set(op_array, "def", dfg->def + (j * dfg->size));
-//???D			zend_jit_dump_var_set(op_array, "use", dfg->use + (j * dfg->size));
-//???D			zend_jit_dump_var_set(op_array, "in ", dfg->in  + (j * dfg->size));
-//???D			zend_jit_dump_var_set(op_array, "out", dfg->out + (j * dfg->size));
-//???D		}
-//???D	}
 
 	return SUCCESS;
 }

ext/opcache/Optimizer/zend_dfg.h
@@ -44,7 +44,7 @@ typedef struct _zend_dfg {
 
 BEGIN_EXTERN_C()
 
-int zend_build_dfg(zend_op_array *op_array, zend_cfg *cfg, zend_dfg *dfg);
+int zend_build_dfg(const zend_op_array *op_array, const zend_cfg *cfg, zend_dfg *dfg);
 
 END_EXTERN_C()
 

commit ac31259301205ecfb9a6f5cd1b735dcb1c6b46ed

Fix memory leak
Leak was introduced in 2737d0cf7cf9e1666c81cb84114f91d94ffa14e8.

ext/opcache/Optimizer/zend_call_graph.c
@@ -260,6 +260,8 @@ static void zend_analyze_recursion(zend_call_graph *call_graph)
 			call_info = call_info->next_caller;
 		}
 	}
+
+	free_alloca(visited, use_heap);
 }
 
 static void zend_sort_op_arrays(zend_call_graph *call_graph)
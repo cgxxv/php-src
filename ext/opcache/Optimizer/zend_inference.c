cgxxv/php-src

commit 3dc6592cf6f54e92456afd7d0e110648fa2e4f0d

Fixed 7.1 build, decls first please!

ext/opcache/Optimizer/zend_inference.c
@@ -3695,12 +3695,14 @@ static zend_bool can_convert_to_double(
 static int zend_type_narrowing(const zend_op_array *op_array, const zend_script *script, zend_ssa *ssa)
 {
 	uint32_t bitset_len = zend_bitset_len(ssa->vars_count);
-	ALLOCA_FLAG(use_heap);
-	zend_bitset visited = ZEND_BITSET_ALLOCA(2 * bitset_len, use_heap);
+	zend_bitset visited = 0;
 	zend_bitset worklist = visited + bitset_len;
 	int i, v;
 	zend_op *opline;
 	zend_bool narrowed = 0;
+	ALLOCA_FLAG(use_heap)
+
+	visited = ZEND_BITSET_ALLOCA(2 * bitset_len, use_heap);
 
 	zend_bitset_clear(worklist, bitset_len);
 

TSRM/tsrm_win32.c
@@ -208,14 +208,13 @@ TSRM_API int tsrm_win32_access(const char *pathname, int mode)
 	DWORD sec_desc_length = 0, desired_access = 0, granted_access = 0;
 	BYTE * psec_desc = NULL;
 	BOOL fAccess = FALSE;
+	realpath_cache_bucket * bucket = NULL;
+	char * real_path = NULL;
 
 	PHP_WIN32_IOUTIL_INIT_W(pathname)
 	if (!pathw) {
 		return -1;
 	}
-
-	realpath_cache_bucket * bucket = NULL;
-	char * real_path = NULL;
 
 	if (mode == 1 /*X_OK*/) {
 		DWORD type;

Zend/zend_execute.c
@@ -625,8 +625,8 @@ static ZEND_COLD void zend_verify_type_error_common(
 		const char **need_msg, const char **need_kind, const char **need_or_null,
 		const char **given_msg, const char **given_kind)
 {
-	*fname = ZSTR_VAL(zf->common.function_name);
 	zend_bool is_interface = 0;
+	*fname = ZSTR_VAL(zf->common.function_name);
 
 	if (zf->common.scope) {
 		*fsep =  "::";

Zend/zend_virtual_cwd.c
@@ -299,8 +299,8 @@ CWD_API int php_sys_stat_ex(const char *path, zend_stat_t *buf, int lstat) /* {{
 	WIN32_FILE_ATTRIBUTE_DATA data;
 	LARGE_INTEGER t;
 	const size_t path_len = strlen(path);
-	ALLOCA_FLAG(use_heap_large);
 	wchar_t *pathw = php_win32_ioutil_any_to_w(path);
+	ALLOCA_FLAG(use_heap_large)
 
 	if (!pathw) {
 		return -1;

ext/standard/array.c
@@ -5112,6 +5112,7 @@ PHP_FUNCTION(array_rand)
 	zend_bitset bitset;
 	int negative_bitset = 0;
 	uint32_t bitset_len;
+	ALLOCA_FLAG(use_heap)
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS(), "a|l", &input, &num_req) == FAILURE) {
 		return;
@@ -5165,7 +5166,6 @@ PHP_FUNCTION(array_rand)
 		num_req = num_avail - num_req;
 	}
 
-	ALLOCA_FLAG(use_heap);
 	bitset_len = zend_bitset_len(num_avail);
 	bitset = ZEND_BITSET_ALLOCA(bitset_len, use_heap);
 	zend_bitset_clear(bitset, bitset_len);

commit 999f6b7a1e17136a9d15249c4d6a02665c94dd28

Fixed segfault introduced in 3dc6592cf6f54e92456afd7d0e110648fa2e4f0d

ext/opcache/Optimizer/zend_inference.c
@@ -3695,14 +3695,14 @@ static zend_bool can_convert_to_double(
 static int zend_type_narrowing(const zend_op_array *op_array, const zend_script *script, zend_ssa *ssa)
 {
 	uint32_t bitset_len = zend_bitset_len(ssa->vars_count);
-	zend_bitset visited = 0;
-	zend_bitset worklist = visited + bitset_len;
+	zend_bitset visited, worklist;
 	int i, v;
 	zend_op *opline;
 	zend_bool narrowed = 0;
 	ALLOCA_FLAG(use_heap)
 
 	visited = ZEND_BITSET_ALLOCA(2 * bitset_len, use_heap);
+	worklist = visited + bitset_len;
 
 	zend_bitset_clear(worklist, bitset_len);
 